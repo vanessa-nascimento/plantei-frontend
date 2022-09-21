afd8832267c23751e2ad6fee94eb766ca3cd2621
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-09-21 09:37:41 -0300

    feat: adicionando regra para carregamento de página inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
-// import Login from './Login'
-// import '../styles/globals.css'
+import LoadingScreen from "../src/components/LoadingScreen";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    setTimeout(() => setLoading(true), 8000);
+  }, []);
   return (
-    <Component {...pageProps} />
+    <>
+      {!loading ? (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
   );
 }
 
