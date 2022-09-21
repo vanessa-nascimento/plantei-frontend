833c378a9a357c44a10356e75979b251e65e1b93
LucasImamura <lucas.ia@outlook.com.br>
2022-09-21 09:46:24 -0300

    Devolve tela de login ao _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import LoadingScreen from "./LoadingScreen";
+import Login from "./Login";
 import React from "react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
-    setTimeout(() => setLoading(true), 8000);
+    setTimeout(() => setLoading(false), 4000);
   }, []);
   return (
     <>
       {!loading ? (
         <React.Fragment>
-          <Component {...pageProps} />
+          <Login {...pageProps} />
         </React.Fragment>
       ) : (
         <LoadingScreen />
