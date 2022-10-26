9d8145f87e896f5eca32e66286aad436a34f5a1d
Vanessa Nascimento <vanascimento.dev@gmail.com>
2022-10-26 10:08:03 -0300

    fix: add img de taioba no public

--- pages/LoadingScreen/index.tsx
+++ pages/LoadingScreen/index.tsx
@@ -1,7 +1,5 @@
 import Image from "../../node_modules/next/image";
 import Main from "../../components/Main";
-import taioba1 from '../../assets/icons/icone_taioba1.svg'
-import taioba2 from '../../assets/icons/icone_taioba2.svg'
 
 import { ContainerLoading, PrincipalImage, StartImage, EndImage } from "./style";
 
@@ -11,13 +9,13 @@ export default function LoadingScreen() {
     <Main backgroundColor="#FFE490" padding="0">
       <ContainerLoading>
         <StartImage>
-          <Image src={taioba1} alt="Ícone plantei" layout="fixed" />
+          <Image src="/icone_taioba1.svg" alt="Ícone plantei" width={149} height={250} layout="fixed" />
         </StartImage>
         <PrincipalImage>
           <Image src="/icone_plantei2.svg" alt="Ícone plantei" width={300} height={259} layout="fill" z-index="1" />
         </PrincipalImage>
         <EndImage>
-          <Image src={taioba2} alt="Ícone plantei" layout="fixed" />
+          <Image src="/icone_taioba2.svg" alt="Ícone plantei" width={160} height={331} layout="fixed" />
         </EndImage>
       </ContainerLoading>
     </Main>
